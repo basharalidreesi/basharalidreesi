eb3b9cbcc32e85592081ccbdfeede24084aa97a6
bashar <basharalidreesi@gmail.com>
2021-04-13 04:10:45 +0300

    u

--- assets/js/main.js
+++ assets/js/main.js
@@ -87,7 +87,7 @@ const bashar = {
                         bashar.lexicon.sparkle.setAttribute("transform", "translate(" + sparkleX + ", " + sparkleY + ") rotate(45)");
                         // bashar.lexicon.sparkle.style.transform = "rotate(" + bashar.util.randomIntBetween(0, 45) + "deg)";
                         let opacificationRate = -4 * ((clampedCursorYRatio - 0.5) ** 2) + 1;
-                        // bashar.lexicon.sparkle.setAttribute("fill-opacity", opacificationRate);
+                        bashar.lexicon.sparkle.setAttribute("fill-opacity", opacificationRate);
                         bashar.lexicon.sparkle.setAttribute("stroke-opacity", opacificationRate);
                 },
         },
