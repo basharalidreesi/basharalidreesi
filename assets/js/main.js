c0e75bdac56f89681372ff3c3acbd5f645454043
bashar <basharalidreesi@gmail.com>
2021-04-13 03:59:58 +0300

    u

--- assets/js/main.js
+++ assets/js/main.js
@@ -84,7 +84,8 @@ const bashar = {
                 acceptSparkle: function(sparkleX, sparkleY, clampedCursorYRatio) {
                         // bashar.lexicon.sparkle.setAttribute("x", sparkleX);
                         // bashar.lexicon.sparkle.setAttribute("y", sparkleY);
-                        bashar.lexicon.sparkle.style.transform = "translate(" + sparkleX + ", " + sparkleY + ") rotate(" + bashar.util.randomIntBetween(0, 45) + "deg)";
+                        bashar.lexicon.sparkle.setAttribute("transform", "translate(" + sparkleX + ", " + sparkleY + ")");
+                        bashar.lexicon.sparkle.style.transform = "rotate(" + bashar.util.randomIntBetween(0, 45) + "deg)";
                         let opacificationRate = -4 * ((clampedCursorYRatio - 0.5) ** 2) + 1;
                         // bashar.lexicon.sparkle.setAttribute("fill-opacity", opacificationRate);
                         bashar.lexicon.sparkle.setAttribute("stroke-opacity", opacificationRate);
